9b2bb2a601f9b15422307e816b18a7d5f69e574e
Molodoi <alexsveshkarev@gmail.com>
2020-09-07 15:21:59 +0300

    some fix

--- JS(hw6)/script.js
+++ JS(hw6)/script.js
@@ -118,25 +118,25 @@
 
 //10.
 
-// var x = +prompt('x', '0');
-// var error;
-//
-// switch (x) {
-//     case 0:
-//         error = 'На ноль делить нельзя';
-//         break;
-//     case 1:
-//         error = 'На единицу делить бессмысленно';
-//         break;
-//     case NaN:
-//         error = 'Не математическая операция';
-//         break;
-//     default :
-//         error = 0;
-// }
-//
-// if (error) {
-//     alert(error);
-// } else {
-//     alert(100 / x);
-// }
+var x = +prompt('x', '0');
+var error;
+
+switch (String(x)) {
+    case '0':
+        error = 'На ноль делить нельзя';
+        break;
+    case '1':
+        error = 'На единицу делить бессмысленно';
+        break;
+    case 'NaN':
+        error = 'Не математическая операция';
+        break;
+    default :
+        error = 0;
+}
+
+if (error) {
+    alert(error);
+} else {
+    alert(100 / x);
+}
